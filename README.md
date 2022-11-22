e0a6ef29fe0a320a63d32e21cf96be9c78d245c4
Kenneth Kjær <kennethjensen1995@gmail.com>
2022-11-22 10:09:49 +0100

    update

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
-## Alpha Images
+# Alpha Images
+### Forecast prices based on image-formatted data
 
-- Price data formatted as image data
-- Predict price movements based on images using CNN models implemented with PyTorch
-- Inspiration: [[1]](https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0212320), [[2]](https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3756587), [[3]](https://dl.acm.org/doi/pdf/10.1145/3383455.3422544), [[4]](https://www.mdpi.com/2079-9292/10/3/287)
+- Predict price movements based on images using CNN models
+  - Flexible to add moving averages and volume bars to an image
+  - Implemented with PyTorch
+
+
 - An unfinished project
 - Let me know if you need to format your price data as images
+
+#### Inspiration
+- [[1]](https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0212320) Forecasting stock prices with a feature fusion LSTM-CNN model using different representations of the same data - Kim T, Kim HY - PLoS ONE 2019 14(2): e0212320.
+- [[2]](https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3756587) (Re-)Imag(in)ing Price Trends - Jingwen Jiang, Bryan Kelly and Dacheng Xiu, forthcoming Journal of Finance.
+- [[3]](https://dl.acm.org/doi/pdf/10.1145/3383455.3422544) Trading via image classification - Naftali Cohen, Tucker Balch, Manuela Veloso - Proceedings of the First ACM International Conference on AI in Finance 2020.
+- [[4]](https://www.mdpi.com/2079-9292/10/3/287) An Advanced CNN-LSTMModel for Cryptocurrency Forecasting - Ioannis E. Livieris, Niki Kiriakidou, Stavros Stavroyiannis and Panagiotis Pintelas - Electronics 2021 10(3) 287.
+
+
+
+
 
--- data_utils.py
+++ data_utils.py
@@ -59,8 +59,18 @@ class ImageBase:
 
         Returns: Pixels for candles
         """
+        min_tmp = np.min(raw_candles)
+        max_tmp = np.max(raw_candles)
 
-        ##
+        if min_tmp == max_tmp:
+            return [], True
+
+        normalized_candles = (raw_candles - min_tmp) / (max_tmp - min_tmp)
+
+        scaled_candles = np.round(normalized_candles * (self.candle_height_pixels - 1)).astype(int)  # subtract one since normalize_candles \in [0, 1] which would give one to many pixels
+        # Highest value is (candle_height_pixels - 1) but highest index in image is 0 (start from top).
+        # Thus, flip pixels to run from 0 (highest position) to (candle_height_pixels - 1) (lowest position)
+        scaled_candles = (self.candle_height_pixels - 1) - scaled_candles
 
         return scaled_candles, False
 
@@ -72,18 +82,53 @@ class ImageBase:
 
         Returns: array with dim (n_periods x 3) x candle_height_pixels
         """
+        # Initialize the image for candles to be zeros
+        candle_image = np.zeros((1, self.candle_height_pixels, self.n_periods * 3))  # 1 for number of channels
 
-        ##
+        for period in range(self.n_periods):
+            # Set open pixel
+            open_col = period * 3
+            candle_image[:, candles[period, 0], open_col] = 1
+            # Set high-low pixels
+            high_low_bar_col = (period * 3) + 1
+            candle_image[:, candles[period, 1]:(candles[period, 2] + 1), high_low_bar_col] = 1
+            # Set close pixel
+            close_col = (period * 3) + 2
+            candle_image[:, candles[period, 3], close_col] = 1
+
+        candle_image = candle_image.astype(np.double)
 
         return candle_image
 
     def place_moving_averages(self, candle_image, prices):
         """
-        Take candle image and place moving averages in candle image.
+        Take candle image and ensure pixels with moving average is 1
         - prices are high, low and moving averages
         """
+        min_tmp = np.min(prices[['high', 'low']].values)
+        max_tmp = np.max(prices[['high', 'low']].values)
 
-        ##
+        normalized_ma = (prices['ma' + str(self.n_periods)].values - min_tmp) / (max_tmp - min_tmp)
+        normalized_ma = np.clip(normalized_ma, 0, 1)  # ensure in [0, 1]: could be above if prices before higher/lower
+
+        scaled_ma = np.round(normalized_ma * (self.candle_height_pixels - 1)).astype(int)  # subtract one since normalize_candles \in [0, 1] which would give one to many pixels
+        scaled_ma = (self.candle_height_pixels - 1) - scaled_ma
+
+        for period in range(self.n_periods):
+            # First
+            if period == 0:
+                candle_image[:, scaled_ma[period], period] = 1  # should ideally take into account previous close not shown in image
+            else:
+                pixel_height = int(np.round(scaled_ma[period - 1] * (1/3) + scaled_ma[period] * (2/3), 0))
+                candle_image[:, pixel_height, period * 3] = 1
+            # Second
+            candle_image[:, scaled_ma[period], period * 3 + 1] = 1
+            # Third
+            if (period + 1) == self.n_periods:  # the last columns
+                candle_image[:, scaled_ma[period], period * 3 + 2] = 1
+            else:
+                pixel_height = int(np.round(scaled_ma[period] * (2/3) + scaled_ma[period + 1] * (1/3), 0))
+                candle_image[:, pixel_height, period * 3 + 2] = 1
 
         return candle_image
 
@@ -91,8 +136,16 @@ class ImageBase:
         """
         Take volumes and return image with scaled volume bars
         """
+        # Initialize the image for candels to be zeros
+        volume_image = np.zeros((1, self.volume_height_pixels, self.n_periods * 3))  # 1 for number of channels
 
-        ##
+        max_tmp = np.max(volumes)
+        normalized_vol = volumes / max_tmp
+        scaled_vol = np.round(normalized_vol * (self.volume_height_pixels - 1)).astype(int)
+        scaled_vol = (self.volume_height_pixels - 1) - scaled_vol
+
+        for period in range(self.n_periods):
+            volume_image[:, scaled_vol[period]:self.volume_height_pixels, period * 3 + 1] = 1
 
         return volume_image
 
@@ -121,22 +174,60 @@ class MakeImages(ImageBase):
         Returns: An image for each date n_periods back with height candle_height_pixels
         """
 
-        ##
+        for t in range(self.n_periods, self.data.shape[0] + 1):
+            tmp_data = self.data.iloc[(t - self.n_periods):t]
+            candles, skip = self.candle_maker(tmp_data.loc[:, ['open', 'high', 'low', 'close']].values)
+            if skip:  # not possible to make candles then remove row
+                self.data.loc[t - 1, 'remove_row'] = True
+                self.data.loc[t - 1, self.img_col_name] = [np.nan]
+                continue
+            #  ...
+            #  day -1  open, high, low, close
+            #  day 0   open, high, low, close
+            candle_image = self.place_candles(candles)
+            if self.ma and not skip:
+                candle_image = self.place_moving_averages(candle_image, tmp_data.loc[:, ['high', 'low', 'ma' + str(self.n_periods)]])
+
+            if self.vb and not skip:
+                volume_image = self.place_volume_bars(tmp_data.loc[:, 'volume'].values)
+                self.data.loc[t - 1, self.img_col_name] = [np.hstack([candle_image, volume_image])]
+            elif not skip:
+                self.data.loc[t - 1, self.img_col_name] = [candle_image]
 
     def make_targets(self, target_type, add_target_to_image=True):
         """
         Returns: Appends target to list with image as first entry and target as second entry.
         """
+        # Align image with target
+        self.data[self.target_price_pred] = self.data[str(self.target_price)].shift(-self.target_p_ahead)
+        # Get if target is up or down and the price diff
+        self.data['direction'] = (self.data[self.target_price_pred] > self.data[self.target_price]).astype(float)
+        self.data['price_diff'] = self.data[self.target_price_pred] - self.data[self.target_price]
+        self.data['return'] = (self.data[self.target_price_pred] - self.data[self.target_price]) / self.data[self.target_price]
+        if target_type == "direction":
+            self.data['target'] = self.data['direction']
+        else:
+            self.data['target'] = self.data['return'] > target_type
 
-        ##
+        if add_target_to_image:
+            for i in range(self.n_periods, self.data.shape[0] + 1):
+                self.data.loc[i - 1, self.img_col_name].append(self.data.loc[i - 1, 'target'])
 
     def make_images_and_targets(self, target_type='direction', return_dataset_class=False):
+        """
+        - target_type takes values "direction" or a float indication the return required for positive (a 1)
+        Returns: A Dataset class with prepared images and targets.
+        """
         self.make_images()
         self.make_targets(target_type)
 
-        """
-        Data checks
-        """
+        # Only remove rows after making targets to not remove needed prices
+        self.data = self.data[~(self.data['remove_row'])]
+        self.data = self.data[~(self.data[self.img_col_name] == -1)]  # remove if not enough prior periods
+        self.data = self.data[~self.data[self.target_price_pred].isna()]  # remove if not enough future periods
+        self.data = self.data[~(np.abs(self.data['return']) > 1)]  # remove if price move more than 100% (data error)
+
+        self.data = self.data.reset_index(drop=True)  # ensure index starts a 0 and runs to len(data)
 
         if return_dataset_class:
             return ImageDataset(self.data, self.n_periods, self.candle_height_pixels)
@@ -170,7 +261,24 @@ class MakeImage(ImageBase):
         Returns: An image with height candle_height_pixels
         """
 
-        ##
+        candles, skip = self.candle_maker(raw_candles.loc[:, ['open', 'high', 'low', 'close']].values)
+
+        if skip:  # not possible to make candles then remove row
+            return None
+            #  ...
+            #  day -1  open, high, low, close
+            #  day 0   open, high, low, close
+        candle_image = self.place_candles(candles)
+
+        if self.ma and not skip:
+            candle_image = self.place_moving_averages(candle_image, raw_candles.loc[:, ['high', 'low', 'ma' + str(self.n_periods)]])
+
+        if self.vb and not skip:
+            volume_image = self.place_volume_bars(raw_candles.loc[:, 'volume'].values)
+            return [np.hstack([candle_image, volume_image])]
+        elif not skip:
+            return [candle_image]
+
 
 def fetch_api_candle_images(setup_dict: dict, return_data=True):
     """
@@ -199,6 +307,7 @@ def fetch_api_candle_images(setup_dict: dict, return_data=True):
     df = df[['timestamp_open', 'open', 'high', 'low', 'close', 'volume', 'timestamp_close']].astype(float)
     df['datetime_open'] = [dt.datetime.fromtimestamp(x / 1000) for x in df.timestamp_open]
     df['datetime_close'] = [dt.datetime.fromtimestamp(x / 1000) for x in df.timestamp_close]
+    df['datetime_close'] = [dt.datetime.fromtimestamp(x / 1000) for x in df.timestamp_close]
     df['year'] = df['datetime_open'].dt.year
     imagine = MakeImages(df, setup_dict['n_periods'], setup_dict['height_pixels'], setup_dict['target_price'],
                          setup_dict['target_p_ahead'], setup_dict['vb'], setup_dict['ma'], 1/5)
@@ -216,7 +325,15 @@ def make_targets(data, target_price_pred, target_price, target_type, target_p_ah
     """
     Returns: Data with price and target columns
     """
-
-    ##
+    # Align image with target
+    data[target_price_pred] = data[str(target_price)].shift(-target_p_ahead)
+    # Get if target is up or down and the price diff
+    data['direction'] = (data[target_price_pred] > data[target_price]).astype(float)
+    data['price_diff'] = data[target_price_pred] - data[target_price]
+    data['return'] = data['price_diff'] / data[target_price]
+    if target_type == "direction":
+        data['target'] = data['direction']
+    else:
+        data['target'] = data['return'] > target_type
 
     return data
